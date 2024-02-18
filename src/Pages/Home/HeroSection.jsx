754572e34eab4fb0e7f02739110dce45310286f4
Vishmi <vishmikalansooriya@gmail.com>
2024-02-18 12:44:32 +0800

    The title

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection()
+{
+    return (
+
+        <section id = "heroSection" className="hero-section">
+            <div className ="hero-section-content-box">
+                <div className ="hero-section-content">
+                    <p className="section-title">
+                        I'm
+                    </p>
+                    <h1 className="hero-section-title">
+                        <span className ="hero-section-title-color">
+                            Vishi K.
+                        </span>{" "}
+                    </h1>
+
+                </div>
+
+            </div>
+            
+        </section>
+    )
+}
