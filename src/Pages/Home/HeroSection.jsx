d4d5cd40478968423257bd855125c7842e17c8c1
Vishmi <vishmikalansooriya@gmail.com>
2024-02-18 20:59:41 +0800

    styled the home page

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,33 +1,22 @@
-export default function HeroSection()
-{
+export default function HeroSection() {
     return (
-
-        <section id = "heroSection" className="hero-section">
-            <div className ="hero-section-content-box">
-                <div className ="hero-section-content">
-                    <p className="section-title">
-                        Hi! I'm
-                    </p>
-                    <h1 className="hero-section-title">
-                        <span className ="hero-section-title-color">
-                            Vishi K.
-                        </span>{" "}
-                    </h1>
-
-                    <p className="hero-section-description">
-                    A recent graduate with a degree in Software Engineering and a passion for crafting digital solutions.
-                    I am actively seeking opportunities to kick-start my career as a junior software engineer. Whether you need a website developed or are looking to add a dynamic team member, I'm eager to contribute my skills and enthusiasm to make an impact.    
-                   </p>
-
-                    <button className="hero-section-btn-primary"> Download resume</button>
-                </div>
-                <img src="./img/me-image.png" alt="Vishi K." />
-
-
-
-
-            </div>
-            
-        </section>
-    )
-}
+      <section id="heroSection" className="hero--section">
+        <div className="hero--section--content--box">
+          <div className="hero--section--content">
+            <p className="section--title">Hi!, I'm </p>
+            <h1 className="hero--section--title">
+              <span className="hero--section-title--color"> Vishi K.</span>{" "}
+            </h1>
+            <p className="hero--section-description">             
+            A recent graduate with a degree in Software Engineering and a passion for crafting digital solutions.
+            I am actively seeking opportunities to kick-start my career as a junior software engineer or a junior devOps Engineer. Whether you need a website developed or are looking to add a dynamic team member, I'm eager to contribute my skills and enthusiasm to make an impact.    
+            </p>
+          </div>
+          <button className="btn btn-primary">Download resume</button>
+        </div>
+        <div className="hero--section--img">
+          <img src="./img/me-image.png" alt="Hero Section" />
+        </div>
+      </section>
+    );
+  }
